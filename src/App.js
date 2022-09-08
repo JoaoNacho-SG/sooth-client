4aa056579f4f43c60c956258c9fcb63a2e6a91c3
Joao Nacho <joao.nacho@studiographene.com>
2022-09-08 15:37:17 +0100

    folder structure added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { Button } from "./components/general/Button"
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+
+<Button />
+
+{/* Make routes here */}
+<Routes>
+  <Route />
+</Routes>
 
     </div>
   );
---
+++ src/components/general/Button/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import style from "./button.module.scss"
+
+export const Button = () => {
+  return (
+    <button className={style.btn}>Click me</button>
+  )
+}

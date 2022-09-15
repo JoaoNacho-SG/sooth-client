10109be7c7ac17019eab4c11b2b0decd3ded6f66
Joao Nacho <joao.nacho@studiographene.com>
2022-09-15 13:33:56 +0100

    login validator done

--- src/components/Pages/Login/LoginForm/index.js
+++ src/components/Pages/Login/LoginForm/index.js
@@ -3,10 +3,13 @@ import { Button } from "../../../general/Button";
 import style from "./loginform.module.scss";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
 export const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [helper, setHelper] = useState(false);
+  const [passVisible, setPassVisible] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -23,12 +26,48 @@ export const LoginForm = () => {
             placeholder="Email Address"
             onChange={(e) => setEmail(e.target.value)}
           />
-          <input
-            type="password"
-            required
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
+
+          <div className={style.input__password}>
+            <input
+              type={!passVisible ? "password" : "text"}
+              required
+              title="minimum 8 characters, 1 uppercase, 1 lowercase, 1 number"
+              pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div
+              className={style.input__visibility}
+              onClick={() => setPassVisible(!passVisible)}
+            >
+              {!passVisible ? <BsEyeFill /> : <BsEyeSlashFill />}
+            </div>
+          </div>
+
+          <p className={style.helper} onClick={() => setHelper(!helper)}>
+            <span>Need help?</span>
+          </p>
+
+          <ul
+            className={
+              helper ? style.helper__text_active : style.helper__text_inactive
+            }
+          >
+            <p>Password must have at least:</p>
+            <li>
+              <p>8 characters;</p>
+            </li>
+            <li>
+              <p>1 uppercase;</p>
+            </li>
+            <li>
+              <p>1 lowercase;</p>
+            </li>
+            <li>
+              <p>1 number.</p>
+            </li>
+          </ul>
+
           <Button btnContent={"Continue"} btnClass={"login"} type="submit" />
           <p>
             <span>Forgot your password?</span>

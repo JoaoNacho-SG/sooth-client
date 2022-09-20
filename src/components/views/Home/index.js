a6a6076c950c68f1d4992f0745208ae096ce62e1
Joao Nacho <joao.nacho@studiographene.com>
2022-09-20 14:53:02 +0100

    products saved in useState instead of localstorage

--- src/components/views/Home/index.js
+++ src/components/views/Home/index.js
@@ -1,27 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../../Navbar";
 import { HeroSection } from "../../Pages/Home/HeroSection";
 import { Explore } from "../../Pages/Home//Explore";
 import { Popular } from "../../Pages/Home//Popular";
-import { CleanProducts } from "../../Pages/Home//CleanProducts";
-import { TopRated } from "../../Pages/Home//TopRated";
-import { NaturalIngredients } from "../../Pages/Home//NaturalIngredients";
-import { BestIngredients } from "../../Pages/Home//BestIngredients";
+import { CleanProducts } from "../../Pages/Home/CleanProducts";
+import { TopRated } from "../../Pages/Home/TopRated";
+import { NaturalIngredients } from "../../Pages/Home/NaturalIngredients";
+import { BestIngredients } from "../../Pages/Home/BestIngredients";
 import { LearnMore } from "../../general/LearnMore";
 import { JoinSooth } from "../../general/JoinSooth";
 import { Footer } from "../../Footer";
 import { getFiftyProducts } from "../../../utils/api";
 
 export const Home = () => {
+  const [productsList, setProductsList] = useState([]);
+
   useEffect(() => {
     (async () => {
       const productsFromApi = await getFiftyProducts();
-      localStorage.setItem("products", JSON.stringify(productsFromApi.data));
+      setProductsList(productsFromApi.data);
     })();
   }, []);
 
   //One product to display in hero section
-  const productsList = JSON.parse(localStorage.getItem("products"));
   const productForHeroSection = productsList[0];
   //4 products to display in popular
   const mostPopular = productsList

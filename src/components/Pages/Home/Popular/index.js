7b2e2aa9a759eebbd59468d0dadfae7749dceda7
Joao Nacho <joao.nacho@studiographene.com>
2022-09-20 12:33:28 +0100

    added tag in card

--- src/components/Pages/Home/Popular/index.js
+++ src/components/Pages/Home/Popular/index.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
 import { Card } from "../../../general/Card";
 import { Layout } from "../../../layout/Layout";
 import style from "./popular.module.scss";
 import { Link } from "react-router-dom";
+import { calcPercentage } from "../../../../utils/percentage";
 
 export const Popular = ({ popularProducts }) => {
   return (
@@ -16,6 +17,53 @@ export const Popular = ({ popularProducts }) => {
 
           <div className={style.popular__container}>
             {popularProducts.map((product) => {
+              if (product.category === "jewelery") {
+                return (
+                  <article key={product.id}>
+                    <Card
+                      img={product.image}
+                      title={product.title}
+                      description={product.description}
+                      price={product.price}
+                      icon={true}
+                      btnContent={`Add to cart`}
+                      btnClass={"primary"}
+                      tagCategory={"jewelery"}
+                    />
+
+                    <Link className="link" to={`/product/${product.id}`}>
+                      <p className={style.details}>more details</p>
+                    </Link>
+                  </article>
+                );
+              }
+
+              if (
+                product.title.toLowerCase().includes("women") &&
+                product.category !== "jewelery"
+              ) {
+                return (
+                  <article key={product.id}>
+                    <Card
+                      img={product.image}
+                      title={product.title}
+                      description={product.description}
+                      discount={true}
+                      price={product.price}
+                      newprice={`${calcPercentage(product.price, 15)}`}
+                      icon={true}
+                      btnContent={`Add to cart`}
+                      btnClass={"primary"}
+                      tagCategory={"women"}
+                    />
+
+                    <Link className="link" to={`/product/${product.id}`}>
+                      <p className={style.details}>more details</p>
+                    </Link>
+                  </article>
+                );
+              }
+
               return (
                 <article key={product.id}>
                   <Card
--- src/components/Pages/Home/TopRated/index.js
+++ src/components/Pages/Home/TopRated/index.js
@@ -3,6 +3,7 @@ import style from "./toprated.module.scss";
 import { Card } from "../../../general/Card";
 import { Layout } from "../../../layout/Layout";
 import { Link } from "react-router-dom";
+import { calcPercentage } from "../../../../utils/percentage";
 
 export const TopRated = ({ topRatedProducts }) => {
   return (
@@ -16,6 +17,53 @@ export const TopRated = ({ topRatedProducts }) => {
 
           <div className={style.toprated__container}>
             {topRatedProducts.map((product) => {
+              if (product.category === "jewelery") {
+                return (
+                  <article key={product.id}>
+                    <Card
+                      img={product.image}
+                      title={product.title}
+                      description={product.description}
+                      price={product.price}
+                      icon={true}
+                      btnContent={`Add to cart`}
+                      btnClass={"primary"}
+                      tagCategory={"jewelery"}
+                    />
+
+                    <Link className="link" to={`/product/${product.id}`}>
+                      <p className={style.details}>more details</p>
+                    </Link>
+                  </article>
+                );
+              }
+
+              if (
+                product.title.toLowerCase().includes("women") &&
+                product.category !== "jewelery"
+              ) {
+                return (
+                  <article key={product.id}>
+                    <Card
+                      img={product.image}
+                      title={product.title}
+                      description={product.description}
+                      discount={true}
+                      price={product.price}
+                      newprice={`${calcPercentage(product.price, 15)}`}
+                      icon={true}
+                      btnContent={`Add to cart`}
+                      btnClass={"primary"}
+                      tagCategory={"women"}
+                    />
+
+                    <Link className="link" to={`/product/${product.id}`}>
+                      <p className={style.details}>more details</p>
+                    </Link>
+                  </article>
+                );
+              }
+
               return (
                 <article key={product.id}>
                   <Card

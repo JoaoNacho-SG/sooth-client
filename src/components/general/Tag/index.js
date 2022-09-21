a6138a2bb311ed26eb31991f444e27a724fc0fa2
Joao Nacho <joao.nacho@studiographene.com>
2022-09-21 13:03:12 +0100

    done product page - Ticket-5 / Product Page

--- src/components/general/Tag/index.js
+++ src/components/general/Tag/index.js
@@ -1,7 +1,21 @@
 import React from "react";
 import style from "./tag.module.scss";
 
-export const Tag = ({ tagContent, tagDiscount }) => {
+export const Tag = ({ tagContent, tagDiscount, productPage }) => {
+  if (productPage) {
+    return (
+      <div
+        className={
+          !tagDiscount
+            ? style.tag__new_productpage
+            : style.tag__discount_productpage
+        }
+      >
+        <p>{tagContent}</p>
+      </div>
+    );
+  }
+
   return (
     <div
       className={
---
+++ src/utils/cart.js
@@ -0,0 +1,1 @@
+export const addToCart = (id, amount) => {};
